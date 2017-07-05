53a4e3daf62b5c0a8601677f3ced1a68ad5db2ab
Patrik (czhujer) Majer <patrik.majer@atlas.cz>
2017-07-05 22:55:58 +0200

    rework defs for apt

--- manifests/repo/apt.pp
+++ manifests/repo/apt.pp
@@ -1,26 +1,38 @@
 # Setup the official repo
 class beats::repo::apt() {
-  apt::source { 'elastic-beats':
-    comment  => 'Official Elastic beats repositories',
-    location => 'https://packages.elastic.co/beats/apt',
+
+  #include apt
+
+  apt::key { 'elasticsearch':
+    id          => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
+    key_source  => 'https://artifacts.elastic.co/GPG-KEY-elasticsearch',
+    require     => Package['apt-transport-https'],
+  }
+
+  apt::source { 'elastic-5.x':
+    location => 'https://artifacts.elastic.co/packages/5.x/apt',
     release  => 'stable',
     repos    => 'main',
-    key      => {
-      id     => '46095ACC8548582C1A2699A9D27D666CD88E42B4',
-      server => 'pgp.mit.edu',
-    },
     include  => {
       'deb'  => true,
     },
+    require  => [
+                 apt::key['elasticsearch'],
+                 Package['apt-transport-https'],
+                ],
   }
-  exec {'apt-get update':
-    command => 'apt-get -qq update',
-    path    => '/usr/bin',
-    unless  => ['/usr/bin/dpkg -l | /bin/grep apt-transport-https'],
-  }
-  package {'apt-transport-https':
-    ensure  => latest,
-    before  => Apt::Source['elastic-beats'],
-    require => Exec['apt-get update'],
+
+#  exec {'apt-get update':
+#    command => 'apt-get -qq update',
+#    path    => '/usr/bin',
+#    unless  => ['/usr/bin/dpkg -l | /bin/grep apt-transport-https'],
+#  }
+
+  if ! defined(Package['apt-transport-https']){
+    package {'apt-transport-https':
+      ensure  => installed,
+#    require => Exec['apt-get update'],
+    }
   }
 }
+
