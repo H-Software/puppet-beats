b60a5699a2476281c691d67953103beb7573950a
Patrik (czhujer) Majer <patrik.majer@atlas.cz>
2017-07-06 14:24:57 +0200

    fix description for metricbeat concat

--- manifests/metricbeat/config.pp
+++ manifests/metricbeat/config.pp
@@ -2,7 +2,7 @@
 class beats::metricbeat::config inherits beats::metricbeat {
   if ($beats::metricbeat::ensure != 'absent'){
     beats::common::headers {'metricbeat':}
-    concat::fragment {'metricbeat.header':
+    concat::fragment {'metricbeat.modules':
       target  => '/etc/metricbeat/metricbeat.yml',
       content => template('beats/metricbeat/metricbeat.yml.erb'),
       order   => '06',
