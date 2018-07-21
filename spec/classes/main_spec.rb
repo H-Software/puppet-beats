48e6d47d08dda4a347cbe58cd1c7fea943703a4e
Patrik (czhujer) Majer <patrik.majer@atlas.cz>
2018-07-21 11:56:35 +0200

    fix rspec tests for debian/ubuntu

--- spec/classes/main_spec.rb
+++ spec/classes/main_spec.rb
@@ -74,7 +74,7 @@ describe 'beats' do
          is_expected.to contain_class('beats::repo::apt')
          is_expected.to contain_class('beats::repo::apt').that_comes_before('beats::package')
 
-         is_expected.to contain_package('geoip-database-extra').with_ensure('latest')
+         is_expected.to contain_package('geoip-database-extra').with_ensure('installed')
          is_expected.to contain_package('apt-transport-https').with_ensure('latest')
          is_expected.to contain_package('libpcap0.8').with_ensure('installed')
        }
@@ -90,7 +90,7 @@ describe 'beats' do
          is_expected.to contain_class('beats::repo::apt')
          is_expected.to contain_class('beats::repo::apt').that_comes_before('beats::package')
 
-         is_expected.to contain_package('geoip-database-extra').with_ensure('latest')
+         is_expected.to contain_package('geoip-database-extra').with_ensure('installed')
          is_expected.to contain_package('apt-transport-https').with_ensure('latest')
          is_expected.to contain_package('libpcap0.8').with_ensure('installed')
        }
@@ -109,7 +109,7 @@ describe 'beats' do
          is_expected.to contain_class('beats::repo::apt')
          is_expected.to contain_class('beats::repo::apt').that_comes_before('beats::package')
 
-         is_expected.to contain_package('geoip-database-extra').with_ensure('latest')
+         is_expected.to contain_package('geoip-database-extra').with_ensure('installed')
          is_expected.to contain_package('apt-transport-https').with_ensure('latest')
          is_expected.to contain_package('libpcap0.8').with_ensure('installed')
        }
